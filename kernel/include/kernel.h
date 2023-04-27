8d44184a7b496e75fbeb4ef838f50e629fc78400
Guada <mgfernandez@frba.utn.edu.ar>
2023-04-27 20:20:30 +0000

    estructura pcb
    Co-authored-by: MoraRodriguez <MoraRodriguez@users.noreply.github.com>
    Co-authored-by: Juan Pablo Castiglione <JuanpiCasti@users.noreply.github.com>
    Co-authored-by: AbrilCaruso <AbrilCaruso@users.noreply.github.com>
    Co-authored-by: Franco Modesto <franco.modesto@hotmail.com>

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -5,8 +5,39 @@
 #include <commons/log.h>
 #include <commons/config.h>
 #include <stdbool.h>
+#include <sys/time.h>
 #include "shared_utils.h"
 #include "tests.h"
 #include "comunicacion.h"
+
+
+typedef struct
+{
+    void* AX;  
+    void* BX;
+    void* CX;
+    void* DX; 
+    void* EAX;
+    void* EBX;
+    void* ECX;
+    void* EDX;
+    void* RAX;
+    void* RBX;
+    void* RCX;
+    void* RDX;
+} t_registros_cpu;
+
+typedef struct
+{
+	uint32_t pid;
+    t_list* instrucciones;
+    uint32_t program_counter;
+    t_registros_cpu registros_cpu;
+    t_list* tabla_segmentos;
+    float estimado_HRRN;
+    time_t tiempo_ready;
+    t_list* archivos_abiertos;
+
+} t_pcb;
 
 #endif
